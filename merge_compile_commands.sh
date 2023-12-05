86a15e0e6fa44e878e03af8334e143eb784e2bfa
Tobias Zaenker <tzaenker@informatik.uni-bonn.de>
2023-12-05 15:10:34 +0100

    Add script to merge compile commands for VS code

---
+++ merge_compile_commands.sh
@@ -0,0 +1,2 @@
+jq -s 'map(.[])' build_release/**/compile_commands.json > build_release/compile_commands.json
+jq -s 'map(.[])' build_debug/**/compile_commands.json > build_debug/compile_commands.json
